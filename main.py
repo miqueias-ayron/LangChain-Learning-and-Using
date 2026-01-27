90fe2c616ffff82394f4c03fafde5d291ee6230e
Miqueias Ayron <miqueiasamf@al.insper.edu.br>
2026-01-27 20:05:39 -0300

    feat: create real search agent with Tavily

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
+from tavily import TavilyClient
+
+tavily = TavilyClient()
 
 @tool
 def search(query:str)->str:
@@ -17,14 +20,13 @@ def search(query:str)->str:
         The search result
     '''
     print(f'Searching for {query}')
-    return 'Tokyo weather is sunny'
+    return tavily.search(query=query)
 
 llm = ChatOpenAI(model='gpt-3.5-turbo', temperature=0)
 tools = [search]
 agent = create_agent(model=llm, tools=tools)
 
 def main():
-    print("Hello from langchain-learning-and-using!")
     result = agent.invoke({'messages':HumanMessage(content='What is weather in Tokyo?')})
     print(result)
 if __name__ == "__main__":
